00a8956beef25312c36c72419e4c8e580f5b5746
ekin <ekin@webbytes.com.my>
2018-11-07 12:58:17 +0800

    First commit

---
+++ WebApiSample/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApiSample.Controllers
+{
+    public class ValuesController : ApiController 
+    {
+        [HttpGet]
+        public IHttpActionResult GetValue()
+        {
+            return Ok("Wow!!!");
+        }
+    }
+}
