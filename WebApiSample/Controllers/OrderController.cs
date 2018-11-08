a9d9197d83b2189af3a2880c98fa8aa0510f38e9
ekin <ekin@webbytes.com.my>
2018-11-08 12:10:06 +0800

    Add Order Controller

---
+++ WebApiSample/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApiSample.Controllers
+{
+    public class OrderController : ApiController 
+    {
+        [HttpGet]
+        public IHttpActionResult GetOrder()
+        {
+            return Ok("Get Successfully.");
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostOrder()
+        {
+            return Ok("Posted.");
+        }
+
+        [HttpPut]
+        public IHttpActionResult PutOrder()
+        {
+            return Ok("Updated.");
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteOrder()
+        {
+            return Ok("Deleted.");
+        }
+    }
+}
