83c3f798410ce117d24e64a6e7f80acf60c79ea1
ekin <ekin@webbytes.com.my>
2018-11-08 10:34:40 +0800

    Added Index Controller

---
+++ WebApiSample/Controllers/IndexController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApiSample.Controllers
+{
+    public class ValuesController : ApiController 
+    {
+        [HttpGet]
+        public IHttpActionResult GetValue()
+        {
+            return Ok("Get Successfully.");
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostValue()
+        {
+            return Ok("Posted.");
+        }
+
+        [HttpPut]
+        public IHttpActionResult PutValue()
+        {
+            return Ok("Updated.");
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteValue()
+        {
+            return Ok("Deleted.");
+        }
+    }
+}
