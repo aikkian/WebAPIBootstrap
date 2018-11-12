cea6f1760cea99cb49c0cba273c4e11903892914
ekin <ekin@webbytes.com.my>
2018-11-12 14:13:05 +0800

    Upgrade .Net Framework 4.6.1 to 4.7

---
+++ WebApiSample/Controllers/:Users:ekin:Projects:WebApiBootstrap:WebApiSample:Controllers:ShipmentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApiSample.Controllers
+{
+    public class ShipmentController : ApiController 
+    {
+        [HttpGet]
+        public IHttpActionResult GetShip()
+        {
+            return Ok("Get Successfully.");
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostOrder()
+        {
+            return Ok("Posted.");
+        }
+
+        [HttpPut]
+        public IHttpActionResult PutOrder()
+        {
+            return Ok("Updated.");
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteOrder()
+        {
+            return Ok("Deleted.");
+        }
+    }
+}
